82292aa4d021ed7e5ce74b927869bbbd4d41eb4c
Yustinia <yustinia@protonmail.com>
2026-06-04 07:04:38 +0800

    add comments on wallhaven for documentation

--- wallhaven.go
+++ wallhaven.go
@@ -9,6 +9,7 @@ var ErrLastPage = errors.New("already on the last page")
 var ErrFirstPage = errors.New("already on first page")
 var ErrInvalidPage = errors.New("not a valid page")
 
+// Search performs the search based on provided search parameters
 func (c *Client) Search(sp SearchParams) (SearchResponse, error) {
 	params := buildParams(sp, c.APIKey)
 
@@ -17,6 +18,7 @@ func (c *Client) Search(sp SearchParams) (SearchResponse, error) {
 	return doRequest[SearchResponse](buildURL)
 }
 
+// GetWallpaperDetails retrieves metadata of a wallpaper provided with the ID
 func (c *Client) GetWallpaperDetails(wallID string) (WallpaperResponse, error) {
 	params := buildBaseParams(c.APIKey)
 
@@ -25,6 +27,7 @@ func (c *Client) GetWallpaperDetails(wallID string) (WallpaperResponse, error) {
 	return doRequest[WallpaperResponse](buildURL)
 }
 
+// NextPage retrieves new results of the next page
 func (c *Client) NextPage(result SearchResponse, sp *SearchParams) (SearchResponse, error) {
 	if result.Metadata.CurrentPage >= result.Metadata.LastPage {
 		return SearchResponse{}, ErrLastPage
@@ -35,6 +38,7 @@ func (c *Client) NextPage(result SearchResponse, sp *SearchParams) (SearchRespon
 	return c.Search(*sp)
 }
 
+// PrevPage retrieves results of the previous page
 func (c *Client) PrevPage(result SearchResponse, sp *SearchParams) (SearchResponse, error) {
 	if result.Metadata.CurrentPage <= 1 {
 		return SearchResponse{}, ErrFirstPage
@@ -45,6 +49,7 @@ func (c *Client) PrevPage(result SearchResponse, sp *SearchParams) (SearchRespon
 	return c.Search(*sp)
 }
 
+// SetPage retrieves results based on the provided page
 func (c *Client) SetPage(result SearchResponse, sp *SearchParams, page int) (SearchResponse, error) {
 	if page > result.Metadata.LastPage {
 		return SearchResponse{}, ErrLastPage
