b7bf97c456ebd247f7cad2f83ab3f5bce0c878be
Yustinia <yustinia@protonmail.com>
2026-06-04 10:22:50 +0800

    add tag methods

---
+++ tags.go
@@ -0,0 +1,36 @@
+package gopaper
+
+// Return tag ID
+func (t Tags) TagID() string {
+	return t.TagID()
+}
+
+// Return tag name
+func (t Tags) TagName() string {
+	return t.Name
+}
+
+// Return alias of tag
+func (t Tags) TagAlias() string {
+	return t.Alias
+}
+
+// Return category id of a tag
+func (t Tags) TagCategoryID() int {
+	return t.CategoryID
+}
+
+// Return category name
+func (t Tags) TagCategory() string {
+	return t.Category
+}
+
+// Return tag content rating
+func (t Tags) TagPurity() string {
+	return t.Purity
+}
+
+// Return tag creation date
+func (t Tags) TagDate() string {
+	return t.Date
+}
